73619ddfefbf275e4a2e114c1db10031da6849c6
jihye <zehye.01@gmail.com>
2019-11-06 15:34:02 +0900

    Second Fizzbuzz.py commit

--- python/191106-fizzbuzz.py
+++ python/191106-fizzbuzz.py
@@ -1,10 +1,5 @@
-for i in range(101):
-	if i % 3 == 0:
-		print('fizz')
-	elif i % 5 == 0:
-		print('buzz')
-	elif i % 15 ==0:
-		print('fizzbuzz')
+for i in range(100):
+	if i % 3 == 0 or i % 5 == 0:
+		print('fizz'*(i%3==0) + 'buzz'*(i*5==0))
 	else:
 		print('nothing')
-
