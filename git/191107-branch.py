89523871825795c6942f6f1b0f941edc8bb25c61
jihye <zehye.01@gmail.com>
2019-11-07 11:42:06 +0900

    modify a git bracnch

--- git/191107-branch.py
+++ git/191107-branch.py
@@ -1,1 +1,9 @@
-print('fizzbuzz starts from here')
+for i in range(100):
+	if i % ==15:
+		print('fizzbuzz')
+	elif i % 3 ==0:
+		print('fizz')
+	elif i % 5 ==0:
+		print('buzz')
+	else: 
+		print(i)
